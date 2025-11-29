0f81f8ff176789525fa111bb2128be897d889586
Rockwell70 <jrshabani@outlook.com>
2025-11-29 11:58:17 -0800

    erste arbeit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Rockwell Shabani on 11/29/25.
+//
+
+#include "hello.h"
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World!\n";
+
+}
---
+++ hello.h
@@ -0,0 +1,8 @@
+//
+// Created by Rockwell Shabani on 11/29/25.
+//
+
+#ifndef CPLUS_HELLO_H
+#define CPLUS_HELLO_H
+
+#endif //CPLUS_HELLO_H
